0b9187b00ca80e192af264f2a2e7ea43d9c80790
Vinicius Ferneda de Lima <vinicius@dynamix.com.br>
2021-05-11 14:53:57 -0300

    first commit

---
+++ app/src/main/java/com/vferneda/contatosbootcamp/Contact.kt
@@ -0,0 +1,3 @@
+package com.vferneda.contatosbootcamp
+
+data class Contact(val name: String, val phoneNumber: String)
---
+++ app/src/main/java/com/vferneda/contatosbootcamp/ContactsAdapter.kt
@@ -0,0 +1,32 @@
+package com.vferneda.contatosbootcamp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactsAdapter(val contactsList: List<Contact>) :
+    RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(contactsList[position])
+    }
+
+    override fun getItemCount(): Int = contactsList.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItem(contact: Contact) {
+            val textName = itemView.findViewById<TextView>(R.id.contact_name)
+            val textPhone = itemView.findViewById<TextView>(R.id.contact_telefone)
+
+            textName.text = contact.name
+            textPhone.text = contact.phoneNumber
+        }
+    }
+}
